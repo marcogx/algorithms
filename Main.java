11fc4de472868e9ba93ee423c4e5acc6c3a46067
gxiong <guang.xiong@hds.com>
2017-08-09 00:51:32 -0700

    work btw my laptop & work one

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        try {
+            Main main = new Main();
+            main.main(new String[] {"Inside"});
+        } catch (Exception e) {
+            System.err.println("not found");
+
+        }
         System.out.println("Hello World!");
     }
 }
---
+++ bfs/LeetCode286.java
@@ -0,0 +1,23 @@
+package bfs;
+
+/**
+ * Created by gxiong on 8/4/17.
+ */
+public class LeetCode286 {
+    public void wallsAndGates(int[][] rooms) {
+//        if (rooms == null || rooms.length == 0 || rooms[0] == null || rooms[0].length == 0) {
+//            return;
+//        }
+        System.out.println("hello");
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+        LeetCode286 s = new LeetCode286();
+        s.wallsAndGates(null);
+    }
+}
