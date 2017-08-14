0dc4a5905655ba66af2e549be16a13ed6f0d0300
gxiong <guang.xiong@hds.com>
2017-08-13 23:45:52 -0700

    Java speed up

--- bfs/Lec286WallAndGates.java
+++ bfs/Lec286WallAndGates.java
@@ -3,7 +3,7 @@ package bfs;
 /**
  * Created by gxiong on 8/4/17.
  */
-public class LeetCode286 {
+public class Lec286WallAndGates {
     public void wallsAndGates(int[][] rooms) {
 //        if (rooms == null || rooms.length == 0 || rooms[0] == null || rooms[0].length == 0) {
 //            return;
@@ -17,7 +17,7 @@ public class LeetCode286 {
 
 
     public static void main(String[] args) {
-        LeetCode286 s = new LeetCode286();
+        Lec286WallAndGates s = new Lec286WallAndGates();
         s.wallsAndGates(null);
     }
 }
---
+++ ds/LinkedList.java
@@ -0,0 +1,7 @@
+package ds;
+
+/**
+ * Created by gxiong on 8/13/17.
+ */
+public class LinkedList {
+}
